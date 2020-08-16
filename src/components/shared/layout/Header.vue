0bc5f85a65c031c0d769f383fc023171db751b77
Isyar <isyarharun@gmail.com>
2020-08-16 18:22:47 +0700

    change menu

--- src/components/shared/layout/Header.vue
+++ src/components/shared/layout/Header.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <a-menu mode="horizontal">
-      <a-menu-item> <a-icon type="mail" />Builder </a-menu-item>
-      <a-menu-item> <a-icon type="appstore" />Responses </a-menu-item>
+      <a-menu-item>Simple Form Builder</a-menu-item>
     </a-menu>
   </div>
 </template>
